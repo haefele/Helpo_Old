90c928333b3906ecc44b008ff2f4f827298630f9
Daniel Häfele <haefele@xemio.net>
2020-12-19 23:08:00 +0100

    Add TitleComponent.razor

---
+++ src/Helpo/Common/DisposableAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Helpo.Common
+{
+    public class DisposableAction : IDisposable
+    {
+        private readonly Action _action;
+
+        public DisposableAction(Action action)
+        {
+            Guard.NotNull(action, nameof(action));
+            
+            this._action = action;
+        }
+        
+        public void Dispose()
+        {
+            this._action();
+        }
+    }
+}
