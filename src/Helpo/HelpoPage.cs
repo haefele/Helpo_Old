5a3d3722991aca22440d9ddff038ecd0b2293d39
Daniel Häfele <haefele@xemio.net>
2020-12-20 17:01:07 +0100

    Create HelpoPage.cs base class for all pages

---
+++ src/Helpo/HelpoPage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Helpo.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace Helpo
+{
+    public abstract class HelpoPage : ComponentBase, IDisposable
+    {
+        private IDisposable? _titleDisposable;
+        
+        [CascadingParameter]
+        public TitleComponent? TitleComponent { get; set; }
+        
+        public virtual string? Title { get; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            if (string.IsNullOrWhiteSpace(this.Title) == false)
+                _titleDisposable = this.TitleComponent?.AddTitle(this.Title);
+        }
+
+        public virtual void Dispose()
+        {
+            this._titleDisposable?.Dispose();
+        }
+    }
+}
